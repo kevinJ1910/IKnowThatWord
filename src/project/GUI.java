8cab074bd6b23de626d85bfc6877ba659019a51b
kevinJ1910 <kevin.jordan@correounivalle.edu.co>
2023-05-28 17:01:49 -0500

    Se agrega plantilla del proyecto

---
+++ src/project/GUI.java
@@ -0,0 +1,64 @@
+package project;
+
+import javax.swing.*;
+import java.awt.*;
+
+/**
+ * This class is used for ...
+ *
+ * @version v.1.0.0 date:28/05/2023
+ * @autor Kevin Jordan Alzate kevin.jordan@correounivalle.edu.co
+ * @autor Junior Cantor Arevalo junior.cantor@correounivalle.edu.co
+ */
+public class GUI extends JFrame {
+
+    private Header headerProject;
+
+    /**
+     * Constructor of GUI class
+     */
+    public GUI() {
+        initGUI();
+
+        //Default JFrame configuration
+        this.setTitle("I Know That Word");
+        this.setSize(300, 300);
+        //this.pack();
+        this.setResizable(true);
+        this.setVisible(true);
+        this.setLocationRelativeTo(null);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+    /**
+     * This method is used to set up the default JComponent Configuration,
+     * create Listener and control Objects used for the GUI class
+     */
+    private void initGUI() {
+        //Set up JFrame Container's Layout
+        //Create Listener Object and Control Object
+        //Set up JComponents
+        headerProject = new Header("I Know That Word", Color.BLACK);
+
+        this.add(headerProject, BorderLayout.NORTH); //Change this line if you change JFrame Container's Layout
+    }
+
+    /**
+     * Main process of the Java program
+     *
+     * @param args Object used in order to send input data from command line when
+     *             the program is execute by console.
+     */
+    public static void main(String[] args) {
+        EventQueue.invokeLater(() -> {
+            GUI miProjectGUI = new GUI();
+        });
+    }
+
+    /**
+     * inner class that extends an Adapter Class or implements Listeners used by GUI class
+     */
+    private class Escucha {
+
+    }
+}
